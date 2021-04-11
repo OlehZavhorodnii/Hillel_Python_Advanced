8dcb4f9d015b08d0835cc4e71538858b2cb22c60
Oleh <ctrlz777@gmail.com>
2021-04-11 22:08:36 +0300

    final Homework 4 - my filter

--- Homework 4 - my filter.py
+++ Homework 4 - my filter.py
@@ -1,17 +1,20 @@
-numbers = [1, 2, 3, 6, 7, 8]
+def get_element(*args):  # get element from the sequence
+    yield from iter(*args)
 
 
-def _filter(*args):
-    yield from _filter(*args)
-    return list()
+def _filter(func, *args):  # select the desired elements from the sequence
+    element = get_element(*args)
+    for n in element:
+        if func(n):
+            yield n
 
 
-n = _filter(numbers)
-print(next(n))
-# print(next(n))
-# print(next(n))
-# print(next(n))
-# print(next(n))
-# print(next(n))
-# print(next(n))
-# print(next(n))
+number_list = range(-5, 5)
+more_than_zero = _filter(lambda x: x > 0, number_list)
+print(more_than_zero)  # print the object of the function
+more_than_zero = list(_filter(lambda x: x > 0, number_list))
+print(more_than_zero)  # print the result of the function
+
+more_than_zero1 = list(filter(lambda x: x > 0, number_list))
+print(more_than_zero1)  # the result of the built-in function filter
+
---
+++ Homework 5 - broken set.py
@@ -0,0 +1,18 @@
+class BadClass:
+
+    def __init__(self, val=0):
+        self.val = val
+
+    def __hash__(self):
+        return hash(self.val)
+
+    def __eq__(self, other):
+        return self.val == other.val
+
+
+bad = BadClass()
+even_worse = frozenset({bad})
+assert(bad in even_worse)
+bad.val += 1
+assert(bad in even_worse)
+print('yeah, we good!')
