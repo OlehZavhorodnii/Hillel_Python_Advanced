ed2e758c9b864eb20e2a33f746c5c1029ec4ac65
Oleh <ctrlz777@gmail.com>
2021-05-10 17:47:01 +0300

    Homework 7 - immutable OOP

--- Homework 7 - immutable OOP.py
+++ Homework 7 - immutable OOP.py
@@ -1,20 +1,20 @@
-
 class Person:
 
     def __init__(self, name, last_name):
         self.__name = name
         self.__last_name = last_name
 
+
     def __str__(self):
         return str(self.__name + ' ' + self.__last_name)
 
 
-student = Person('John', 'Palmer')
+student = Person('John', 'Travolta')
 print(student)
 print(id(student))
 print(hash(student))
 
-student = Person('Will', 'Smith')
+student = Person('Will', 'Jason')
 print(student)
 print(id(student))
 print(hash(student))
