8cab7f3b54f51f8c4aceddccb8d88173a38d2b7e
Oleh <ctrlz777@gmail.com>
2021-05-11 23:37:55 +0300

    addition Homework 7 - immutable OOP

--- Homework 7 - immutable OOP.py
+++ Homework 7 - immutable OOP.py
@@ -4,17 +4,28 @@ class Person:
         self.__name = name
         self.__last_name = last_name
 
-
+    # output to string
     def __str__(self):
         return str(self.__name + ' ' + self.__last_name)
 
+    # method for updating object data
+    def update(self, name, last_name):
+        self.__name = name
+        self.__last_name = last_name
+        return self.__name, self.__last_name
 
-student = Person('John', 'Travolta')
+
+student = Person('John', 'Travolta')  # create object
 print(student)
 print(id(student))
 print(hash(student))
 
-student = Person('Will', 'Jason')
+student.update('James', 'Bond')  # updating object data
+print(student)
+print(id(student))
+print(hash(student))
+
+student = Person('Otto', 'Bismark')  # new object
 print(student)
 print(id(student))
 print(hash(student))
