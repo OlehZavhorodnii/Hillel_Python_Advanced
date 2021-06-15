1a14fcbd5c41537f7bb101fc3073285420c77d93
Oleh <ctrlz777@gmail.com>
2021-06-15 15:05:16 +0300

    fix with remarks

--- Homework 8 - logger.py
+++ Homework 8 - logger.py
@@ -1,49 +1,67 @@
 import json
 import os
-import sys
+from datetime import datetime
 
 
 class Logger:
 
-    def _log(self, level='', msg=''):
-        return json.dumps({'level': level, 'msg': msg})
+    def _config(self, level='ERROR', msg=None, time=datetime.today().isoformat(), **kwargs):
+        params = {  # a dictionary that stores logging settings
+            'level': level,
+            'msg': msg,
+            'time': time
+        }
+        params.update(kwargs)  # expanding the dictionary with additional logging parameters
+        return params
 
-    def info(self, msg):
-        return self._log('INFO', msg)
+    def _log(self, params):  # serialization the dictionary {params} to JSON
+        json.dumps(params)
 
-    def warn(self, msg):
-        return self._log('WARNING', msg)
+    # logging methods:
+    def info(self, msg, **kwargs):  # **kwargs - additional logging parameters
+        self._log(self._config('INFO', msg, **kwargs))
 
-    def error(self, msg):
-        return self._log('ERROR', msg)
+    def warn(self, msg, **kwargs):
+        self._log(self._config('WARNING', msg, **kwargs))
 
-
-class StdOutLogger(Logger):
-
-    def _log(self, level='', msg=''):
-        stdout = json.dumps({'level': level, 'msg': msg})
-        return sys.stdout.write(stdout + '\n')
+    def error(self, msg, **kwargs):
+        self._log(self._config('ERROR', msg, **kwargs))
 
 
-class FileLogger(Logger):
+class StdOutLogger(Logger):  # output logging to the console
 
-    def _log(self, level='', msg=''):
-        to_json = json.dumps({'level': level, 'msg': msg})
-        if os.getcwd():
-            logfile = open('log.json', 'a')
-            logfile.write(to_json + '\n')
+    def _log(self, params):
+        stdout = json.dumps(params)
+        print(stdout)
+
+
+class FileLogger(Logger):  # write logging to file
+
+    def __init__(self, name_dir=os.getcwd(), name_file='log'):
+        self.name_dir = name_dir
+        self.name_file = name_file
+
+    def _log(self, params):
+        to_file = json.dumps(params)
+
+        if os.path.isdir(self.name_dir):
+            os.chdir(self.name_dir)
+        else:
+            os.makedirs(self.name_dir)
+            os.chdir(self.name_dir)
+
+        with open(f'{self.name_file}.json', 'a') as f:
+            f.write(to_file + '\n')
 
 
 log = StdOutLogger()
 
-log.info('This is sparta info')
-log.warn('This is sparta warn')
-log.error('This is sparta error')
+log.info('Info', newparam='add new param')
+log.warn('Warn', newparam='add new param')
+log.error('Error', newparam='add new param')
 
-log = FileLogger()
+log_write = FileLogger('/home/oleh/Study/Write', 'My_log')
 
-log.info('This is sparta info')
-log.warn('This is sparta warn')
-log.error('This is sparta error')
-
-
+log_write.info('Info', newparam='add new param')
+log_write.warn('Warn', newparam='add new param')
+log_write.error('Error', newparam='add new param')
